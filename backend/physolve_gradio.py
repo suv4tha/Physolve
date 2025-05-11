14a616c2f2d54b7fdde5f74d3068a28d55d2d064
Suvetha Oudearadjou <104441612+suv4tha@users.noreply.github.com>
2025-05-11 19:33:14 +0530

    Update physolve_gradio.py

--- backend/physolve_gradio.py
+++ backend/physolve_gradio.py
@@ -60,3 +60,5 @@ with gr.Blocks(theme=gr.themes.Soft(primary_hue="violet")) as app:
     calculate_btn.click(fn=calculate, inputs=[selected_formula] + input_components, outputs=[output])
 
 app.launch()
+if __name__ == "__main__":
+    app.launch(server_name="0.0.0.0", server_port=8080)
